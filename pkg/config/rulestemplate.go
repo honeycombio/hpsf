74a2d6eb309b45b1d9ee5cbdd69753bbe380c2bb
Kent Quirk <kentquirk@honeycomb.io>
2025-05-20 15:01:35 -0400

    Respond to feedback

--- pkg/config/rulestemplate.go
+++ pkg/config/rulestemplate.go
@@ -63,9 +63,9 @@ func (r *rulesCondition) Render(prefix string) (map[string]any, error) {
 // separated by semicolons, e.g. "k=v;k2=v2;k3=v3"
 // the key-value pairs can be either a single value or a list of values
 // separated by commas, e.g. "k=v" or "ks=v1,v2,v3"
-func splitCondition(v any) *rulesCondition {
+func splitCondition(condition any) *rulesCondition {
 	// if the value is a string, split it by semicolons
-	if s, ok := v.(string); ok {
+	if s, ok := condition.(string); ok {
 		parts := strings.Split(s, ";")
 		m := make(map[string]any)
 		for _, part := range parts {
@@ -75,13 +75,14 @@ func splitCondition(v any) *rulesCondition {
 			}
 			k := strings.TrimSpace(kv[0])
 			v := strings.TrimSpace(kv[1])
-			if strings.Contains(v, ",") {
+			if k == "fs" {
 				m[k] = strings.Split(v, ",")
 			} else {
 				m[k] = v
 			}
 		}
 
+		// only the fs case can be a []strings, every other v is a string
 		cond := &rulesCondition{index: -1}
 		for k, v := range m {
 			switch k {
@@ -111,8 +112,8 @@ func splitCondition(v any) *rulesCondition {
 	return nil
 }
 
-func buildRulesTemplate(t TemplateData) (rulesTemplate, error) {
-	r := rulesTemplate{kvs: make([]dottedConfigTemplateKV, 0)}
+func buildRulesTemplate(t TemplateData) (*rulesTemplate, error) {
+	r := &rulesTemplate{kvs: make([]dottedConfigTemplateKV, 0)}
 
 	for mk, mv := range t.Meta {
 		var ok bool
@@ -134,6 +135,9 @@ func buildRulesTemplate(t TemplateData) (rulesTemplate, error) {
 
 	if r.env == "" {
 		return r, fmt.Errorf("missing env in meta")
+	}
+	if r.sampler == "" {
+		return r, fmt.Errorf("missing sampler in meta")
 	}
 
 	for _, d := range t.Data {
@@ -170,24 +174,20 @@ func buildRulesTemplate(t TemplateData) (rulesTemplate, error) {
 	return r, nil
 }
 
-func (t *TemplateComponent) generateRulesConfig(rt rulesTemplate, userdata map[string]any) (*tmpl.RulesConfig, error) {
+func (t *TemplateComponent) generateRulesConfig(rt *rulesTemplate, userdata map[string]any) (*tmpl.RulesConfig, error) {
 	dc := tmpl.NewDottedConfig(nil)
-	if strings.Contains(rt.env, "{{") {
-		// if the env contains a template variable, we need to expand it
-		env, err := t.expandTemplateVariable(rt.env, userdata)
-		if err != nil {
-			return nil, err
-		}
-		rt.env = env
+
+	env, err := t.expandTemplateVariable(rt.env, userdata)
+	if err != nil {
+		return nil, err
 	}
-	if strings.Contains(rt.sampler, "{{") {
-		// if the sampler contains a template variable, we need to expand it
-		sampler, err := t.expandTemplateVariable(rt.sampler, userdata)
-		if err != nil {
-			return nil, err
-		}
-		rt.sampler = sampler
+	rt.env = env
+
+	sampler, err := t.expandTemplateVariable(rt.sampler, userdata)
+	if err != nil {
+		return nil, err
 	}
+	rt.sampler = sampler
 
 	keyPrefix := "Samplers." + rt.env + "." + rt.sampler + "."
 	for _, kv := range rt.kvs {
