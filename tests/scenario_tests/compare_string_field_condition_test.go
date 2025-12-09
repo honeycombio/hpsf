e5b3aa4d52e7e222a7a4c0fd853ffa5a5d9bca0b
Mike Goldsmith <goldsmith.mike@gmail.com>
2025-12-09 13:59:13 +0000

    feat: Add new operators to string compare condition

---
+++ tests/scenario_tests/compare_string_field_condition_test.go
@@ -0,0 +1,316 @@
+package hpsftests
+
+import (
+	"testing"
+
+	collectorprovider "github.com/honeycombio/hpsf/tests/providers/collector"
+	hpsfprovider "github.com/honeycombio/hpsf/tests/providers/hpsf"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCompareStringEquals(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_equals.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"status"}, rule.Conditions[0].Fields)
+	assert.Equal(t, "=", rule.Conditions[0].Operator)
+	assert.Equal(t, "success", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringNotEquals(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_not_equals.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"status"}, rule.Conditions[0].Fields)
+	assert.Equal(t, "!=", rule.Conditions[0].Operator)
+	assert.Equal(t, "error", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringContains(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_contains.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"message"}, rule.Conditions[0].Fields)
+	assert.Equal(t, "contains", rule.Conditions[0].Operator)
+	assert.Equal(t, "timeout", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringDoesNotContain(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_does_not_contain.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"message"}, rule.Conditions[0].Fields)
+	assert.Equal(t, "does-not-contain", rule.Conditions[0].Operator)
+	assert.Equal(t, "debug", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringStartsWith(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_starts_with.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"endpoint"}, rule.Conditions[0].Fields)
+	assert.Equal(t, "starts-with", rule.Conditions[0].Operator)
+	assert.Equal(t, "/api/", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringGreaterThan(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_greater_than.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"version"}, rule.Conditions[0].Fields)
+	assert.Equal(t, ">", rule.Conditions[0].Operator)
+	assert.Equal(t, "1.0.0", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringGreaterThanOrEqual(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_greater_than_or_equal.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"version"}, rule.Conditions[0].Fields)
+	assert.Equal(t, ">=", rule.Conditions[0].Operator)
+	assert.Equal(t, "2.0.0", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringLessThan(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_less_than.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"priority"}, rule.Conditions[0].Fields)
+	assert.Equal(t, "<", rule.Conditions[0].Operator)
+	assert.Equal(t, "medium", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
+
+func TestCompareStringLessThanOrEqual(t *testing.T) {
+	rulesConfig, collectorConfig, _ := hpsfprovider.GetParsedConfigsFromFile(t, "testdata/compare_string_less_than_or_equal.yaml")
+
+	tracesPipelineNames := collectorprovider.GetPipelinesByType(collectorConfig, "traces")
+	assert.Len(t, tracesPipelineNames, 1)
+
+	receivers, processors, exporters, getResult := collectorprovider.GetPipelineConfig(collectorConfig, tracesPipelineNames[0].String())
+	require.True(t, getResult.Found)
+
+	assert.Len(t, receivers, 1)
+	assert.Contains(t, receivers, "otlp/Receive_OTel_1")
+
+	assert.Len(t, processors, 1)
+	assert.Contains(t, processors, "usage")
+
+	assert.Len(t, exporters, 1)
+	assert.Contains(t, exporters, "otlphttp/Start_Sampling_1")
+
+	assert.Len(t, rulesConfig.Samplers, 1)
+	defaultSampler, exists := rulesConfig.Samplers["__default__"]
+	require.True(t, exists)
+
+	require.NotNil(t, defaultSampler.RulesBasedSampler)
+	assert.Len(t, defaultSampler.RulesBasedSampler.Rules, 1)
+
+	rule := defaultSampler.RulesBasedSampler.Rules[0]
+	assert.Equal(t, "Sample_All_1", rule.Name)
+	assert.Len(t, rule.Conditions, 1)
+	assert.Equal(t, []string{"priority"}, rule.Conditions[0].Fields)
+	assert.Equal(t, "<=", rule.Conditions[0].Operator)
+	assert.Equal(t, "high", rule.Conditions[0].Value)
+	assert.Equal(t, "string", rule.Conditions[0].Datatype)
+}
